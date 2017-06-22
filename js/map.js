26a62e115efba57f0f2e14715e51affc539124a5
Shui Yu <shuiyu1231@hotmail.com>
2017-06-21 23:11:54 -0700

    Created a time series chart and added a js and css folder.

---
+++ js/map.js
@@ -0,0 +1,234 @@
+!(function (d3) {
+
+$("acontent").empty();
+
+var width = 1200,
+    height = 700;
+
+//var terror_data;
+var parseDate = d3.time.format("%e-%b-%y").parse;
+var formatTime = d3.time.format("%B %d, %Y");
+
+// arrange the layout the visualization
+d3.select("acontent").append("p")
+  .attr("id", "intro")
+  .html("Click the play button to see the animation."+ "<br/>" +
+        "Click the pause button to investigate the details of terrorist attacks of a particular year."+
+        "<br/>" +"Circle size represents the number of casualties in the terrorist attack.");
+
+d3.select("acontent").append("div")
+   .attr("id", "slider");
+
+d3.select("acontent").append("p")
+   .attr("id", "hint");
+
+d3.select("acontent").append("div")
+   .attr("id", "vis");
+
+//<p id="ref">Acknowledgement: "National Consortium for the Study of Terrorism and Responses to Terrorism (START). (2016). 
+//Global Terrorism Database [Data file]. <br/> Retrieved from <a href="https://www.kaggle.com/START-UMD/gtd">https://www.kaggle.com/START-UMD/gtd</a> "</p>
+d3.select("acontent").append("p")
+    .attr("id", "ref")
+    .html("Source: Global Terrorism Database" + "<br/>" +
+         "Acknowledgement: National Consortium for the Study of Terrorism and Responses to Terrorism (START)." + "<br/>"
+         + "Retrieved from https://www.kaggle.com/START-UMD/gtd")
+// Ordinal color scale
+var fill = d3.scale.ordinal().domain(["No", "Yes"]).range([ "#6CA9D6", "#F29319"]);
+
+// set projection
+var projection = d3.geo.albersUsa()
+                       .translate([width/2, height/2])    // translate to center of screen
+                       .scale([1200]); 
+
+// create path variable
+var geoPath = d3.geo.path( ).projection( projection );
+
+// Define Zoom Function Event Listener
+function zoomFunction() { 
+    d3.select("#map")
+        .attr("transform", 
+              "translate(" + d3.event.translate
+                           + ") scale ("
+                           + d3.event.scale + ")");
+};
+
+// Define Zoom Behavior
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 4])
+    .on("zoom", zoomFunction);
+
+// Define tooltip functions
+
+var tip = d3.tip()
+          .attr('class', 'd3-tip')
+          .direction('e')
+          .offset([40, 5])
+          .html(function(d) {
+            return "<strong>Date: </strong>" + formatTime(d.date) + 
+            "<br/>" + "<strong>State: </strong>" + d.state +"<br/>" + 
+            "<strong>City: </strong>" + d.city + "<br/>" + "<strong>Attack Type: </strong>" 
+            + d.attack_type +"<br/>" + "<strong>Weapon: </strong>" + d.weapon +"<br/>"+ 
+            "<strong>Target: </strong>" + d.target +"<br/>"+ "<strong>Group: </strong>" + 
+            d.group +"<br/>"+ "<strong>Fatalities: </strong>" + d.fatalities +"<br/>"+ 
+            "<strong>Injuries: </strong>" + d.injuries + "<br/>" + "<strong>Summary: </strong>" + d.summary;
+          })
+
+// create svg variable
+var svgContainer = d3.select( "#vis" )
+                    .append( "svg" )
+                     .attr( "width", width )
+                     .attr( "height", height )
+                     .call(tip); 
+                     
+// Define group for both the map and dots
+var mapGroup = svgContainer.append("g")
+                   .attr("id", "map")
+                   .attr("transform", "translate(" + 30 + "," + -80 + ")")
+                   .call(zoom);
+
+// Define the div for the tooltip
+var div = d3.select("#vis").append("div")   
+    .attr("class", "tooltip");          
+
+
+// define legend
+var legendRectSize = 15, legendSpacing = 5;
+
+var legendTitle = svgContainer.append("g")
+                           .attr("transform", "translate(" + (width-360) +","+ (height-670) + ")")
+                          .append("text")
+                           .attr("class", "legendText")
+                           .attr("font-family", "sans-serif")
+                           .attr("font-size", "13px")
+                           .text("Any fatalities?");
+
+var legend = svgContainer.append("g")
+            .attr("transform", "translate(" + (width - 350) + "," + (height - 660) + ")" )
+              .selectAll("g")
+              .data(fill.domain())
+              .enter()
+            .append("g")
+              .attr("class", "legend")
+              .attr('transform', function(d, i) {
+                var height = legendRectSize;
+                var x = 0;
+                var y = i * 20 + 5;
+                return 'translate(' + x + ',' + y + ')';
+            });
+
+legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', fill)
+    .style('stroke', fill);
+
+legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - 2)
+    .attr("class", "legendText")
+    .text(function(d) { return d; })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "10px");
+              
+
+// Create a slider to provide a more granular way to investigate the terror attacks by year
+var startYear = 1970,
+    endYear = 2015,
+    currentYear = startYear;
+
+
+// Load data
+d3.json("data/us-states.json", function (error, json) {
+      if (error) return console.warn(error);
+
+      mapGroup.append("path").attr("d", geoPath(json))
+                    .style("fill", "#d9d9d9")
+                    .style("stroke", "#fff")
+                    .style("stroke-width", "1");
+
+      function plot_points( data ) {
+
+              // Clean data formats
+              data.forEach(function(d) {
+                  d.date = parseDate(d.date);
+                  d.year = +d.year
+                  d.fatalities = +d.fatalities;
+                  d.injuries = +d.injuries;
+                  d.totalVictims = d.fatalities + d.injuries; // The new "total victims" column
+                  if (d.fatalities === 0) { return d.anyFatalities = "No"; } 
+                  else { return d.anyFatalities = "Yes"; }; // Identify whether or not the terror attacks caused any fatalities
+              });
+
+              // var terror_data = data;
+
+              var totalVictims_max = d3.max( data, function( d ) {
+                  return d.totalVictims;
+              } );
+
+
+              var radius = d3.scale.sqrt( ).domain( [ 0, totalVictims_max ] ).range( [ 5, 20 ] );
+
+              // Define update function to filter data by year
+              function update() {
+                      var filtered = data.filter(function(d) {
+                          return d.year === currentYear;
+                      });
+
+                      // Update the hint message by year
+                      d3.select('#hint')
+                          .text(" Terrorist Attacks in " + currentYear);
+
+                      // Append circles on the map
+                      var circles = mapGroup.selectAll('circle')
+                               .data( filtered ).enter()
+                              .append('circle')
+                               .attr('class', 'circles')
+                               .style( "fill", function(d) { return fill(d.anyFatalities); } )
+                               .style( 'stroke', 'grey' )
+                               .style( 'stroke-width', 0.3 )
+                               .style("opacity", 0)
+                               .attr( 'cx', function( d ) {
+                                  return projection([+d.longitude, +d.latitude])[ 0 ];
+                              } ).attr( 'cy', function( d ) {
+                                  return projection([+d.longitude, +d.latitude])[ 1 ];
+                              } ).attr( 'r', function( d ) {
+                                  return radius( +d.totalVictims);
+                              } )
+                              .transition()
+                               .style("opacity", 0.7)
+                               .duration(200)
+              };
+              // Create a slider to play the animation
+              var myslider =  chroniton()
+                    .domain([new Date(startYear, 1, 1), new Date(endYear, 1, 1)])
+                    .labelFormat(function(date) {
+                        return date.getFullYear();  
+                     })
+                    .width(800)
+                    .on("change", function(date) {
+                       var newYear = date.getFullYear();
+                       if (newYear != currentYear) { 
+                              currentYear = newYear;
+                              mapGroup.selectAll('circle')
+                                      .remove();
+                              update(); // Call the update function
+                              mapGroup.selectAll('circle').on("mouseover", function(d) {
+                                  tip.show(d);
+                                  d3.select(this).style('stroke', 'white').style('stroke-width', 2);
+                              }) .on("mouseout", function(d) {
+                                  tip.hide(d);
+                                  d3.select(this).style('stroke', 'grey').style('stroke-width', 0.3)
+                              });
+                            }
+                    })
+                    .keybindings(true)
+                    .playButton(true)
+                    .playbackRate(0.2)
+
+              d3.select("#slider").call(myslider);
+     };
+
+    d3.csv( "data/terrorism_usa.csv", plot_points );
+} );
+
+}) (d3);
---
+++ js/time-series.js
@@ -0,0 +1,274 @@
+!(function (d3) {
+
+$("bcontent").empty();
+
+//var checkData;
+var margin = {top: 60, right: 200, bottom: 20, left: 150},
+    width = 1060 - margin.left - margin.right,
+    height = 570 - margin.top - margin.bottom;
+
+var parseYear = d3.time.format("%Y").parse,
+    formatYear = d3.time.format("%Y");
+
+var bisectYear = d3.bisector(function(d) { return d.year; }).left;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+// Define color scale
+var color = d3.scale.ordinal()
+         .range(["#3957ff", "#d3fe14", "#c9080a", "#fec7f8", "#0b7b3e", "#0bf0e9", "#c203c8", "#fd9b39", 
+          "#888593", "#906407", "#98ba7f", "#fe6794", "#10b0ff", "#ac7bff", "#fee7c0", "#964c63", "#1da49c", 
+          "#0ad811", "#bbd9fd", "#fe6cfe", "#297192", "#d1a09c", "#78579e", "#81ffad", "#739400", "#ca6949", 
+          "#d9bf01", "#646a58", "#d5097e", "#bb73a9", "#ccf6e9", "#9cb4b6", "#b6a7d4", "#9e8c62", "#6e83c8", 
+          "#01af64", "#a71afd", "#cfe589", "#d4ccd1", "#fd4109", "#bf8f0e", "#2f786e", "#4ed1a5", "#d8bb7d", 
+          "#a54509", "#6a9276", "#a4777a", "#fc12c9", "#606f15", "#3cc4d9", "#f31c4e"])
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .interpolate("monotone")
+    .x(function (d) { return x(d.year); })
+    .y(function (d) { return y(d.numberOfAttacks); });
+
+var maxY; // Defined later to update yAxis
+
+var svg = d3.select("bcontent").append("svg")
+    .attr("width",  width  + margin.left + margin.right)
+    .attr("height", height + margin.top  + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Create invisible rect for mouse tracking
+svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)                                    
+    .attr("x", 0) 
+    .attr("y", 0)
+    .attr("id", "mouse-tracker")
+    .style("fill", "white");
+
+
+//Load the data
+d3.csv("data/time_series_by_state.csv", function (error, data) {
+
+    var varNames = d3.keys(data[0]).filter(function (key) { return key !== 'year';});
+    color.domain(varNames);
+
+   // Change data formats
+    data.forEach(function(d) {
+      d.year = parseYear(d.year);
+    })
+    
+    // Reformat the data 
+    var seriesData = varNames.map(function (name) {
+      return {
+        name: name,
+        values: data.map(function (d) {
+          return {
+            year: d.year, 
+            numberOfAttacks: +(d[name]),
+          };
+        }),
+        visible: (name === "California" ? true : false)
+      };
+    });
+
+        //checkData = seriesData;
+
+    x.domain(d3.extent(data, function(d) { return d.year; }));
+
+    y.domain([0,
+      d3.max(seriesData, function (c) { 
+        return d3.max(c.values, function (d) { return d.numberOfAttacks; });
+      })]
+    );
+
+      // Draw line graph
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + (height) + ")")
+          .call(xAxis)
+          .selectAll("text")  
+            .style("text-anchor", "middle")
+            .style("font-size", "10px")
+            .style("font-family", "sans-serif")
+            .attr("dx", "-.2em")
+            .attr("dy", "1em");
+        
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", -50)
+          .attr("x", -230)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Number of Terrorist Attacks");
+
+      // Draw grid lines
+      svg.append("g")         
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis
+            .tickSize(-height, 0, 0)
+            .tickFormat("")
+        )
+
+      // Draw the lines
+      var series = svg.selectAll(".series")
+          .data(seriesData)
+        .enter().append("g")
+          .attr("class", "series");
+
+      series.append("path")
+        .attr("class", "line")
+        .attr("id", function(d) {
+          return "line-" + d.name;
+        })
+        .attr("d", function (d) { 
+          return d.visible ? line(d.values) : null; 
+        })
+        .style("stroke", function (d) { 
+          return color(d.name); 
+        });
+    
+      // draw legend
+      var legendSpace = 500 / 51;     
+
+      series.append("rect")
+          .attr("width", 7)
+          .attr("height", 7)                                    
+          .attr("x", width + (margin.right/3)) 
+          .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace) - 8; })  // spacing
+          .attr("fill",function(d) {
+            return d.visible ? color(d.name) : "#F1F1F2"; // If array key "visible" = true then color rect, if not then make it grey 
+          })
+          .attr("class", "legend-box")
+          .on("click", function(d){ // On click make d.visible 
+            d.visible = !d.visible; // If array key for this data selection is "visible" = true then make it false, if false then make it true
+
+            maxY = findMaxY(seriesData); // Find max Y rating value categories data with "visible"; true
+            y.domain([0,maxY]); // Redefine yAxis domain based on highest y value of categories data with "visible"; true
+            svg.select(".y.axis")
+              .transition()
+              .call(yAxis);   
+
+            series.select("path")
+              .transition()
+              .attr("d", function(d){
+                return d.visible ? line(d.values) : null; // If d.visible is true then draw line for this d selection
+              })
+
+            series.select("rect")
+              .transition()
+              .attr("fill", function(d) {
+              return d.visible ? color(d.name) : "#F1F1F2";
+            });
+          })
+
+          .on("mouseover", function(d){
+
+                d3.select(this)
+                  .transition()
+                  .attr("fill", function(d) { return color(d.name); });
+
+                d3.select("#line-" + d.name)
+                  .transition()
+                  .style("stroke-width", 2.5);  
+          })
+
+          .on("mouseout", function(d){
+
+                d3.select(this)
+                  .transition()
+                  .attr("fill", function(d) {
+                  return d.visible ? color(d.name) : "#F1F1F2";});
+
+                d3.select("#line-" + d.name)
+                  .transition()
+                  .style("stroke-width", 1.5);
+          })
+
+        series.append("text")
+            .attr("x", width + (margin.right/3) + 15) 
+            .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace); }) 
+            .text(function(d) { return d.name; }); 
+
+        // Hover line 
+        var hoverLineGroup = svg.append("g") 
+                  .attr("class", "hover-line");
+
+        var hoverLine = hoverLineGroup // Create line with basic attributes
+              .append("line")
+                  .attr("id", "hover-line")
+                  .attr("x1", 10).attr("x2", 10) 
+                  .attr("y1", 0).attr("y2", height + 10)
+                  .style("pointer-events", "none") // Stop line interferring with cursor
+                  .style("opacity", 1e-6); // Set opacity to zero 
+
+        var columnNames = d3.keys(data[0]).slice(1); 
+
+        var focus = series.select("g") // create group elements to house tooltip text
+                          .data(columnNames) // bind each column name date to each g element
+                          .enter()
+                        .append("g") //create one <g> for each columnName
+                          .attr("class", "focus"); 
+
+        focus.append("text") 
+              .attr("class", "ts-tooltip")
+              .attr("x", width + 20) // position tooltips  
+              .attr("y", function (d, i) { return (legendSpace)+i*(legendSpace); });       
+
+        // Add mouseover events for hover line.
+        d3.select("#mouse-tracker") // select chart plot background rect #mouse-tracker
+        .on("mousemove", mousemove) // on mousemove activate mousemove function defined below
+        .on("mouseout", function() {
+            d3.select("#hover-line")
+                .style("opacity", 1e-6); // On mouse out making line invisible
+        });
+
+        function mousemove() { 
+            var mouse_x = d3.mouse(this)[0]; // Finding mouse x position on rect
+            var graph_x = x.invert(mouse_x); 
+            
+            d3.select("#hover-line") // select hover-line and changing attributes to mouse position
+                .attr("x1", mouse_x) 
+                .attr("x2", mouse_x)
+                .style("opacity", 1); // Making line visible
+
+            // Legend tooltips 
+            var x0 = x.invert(d3.mouse(this)[0]), 
+                  i = bisectYear(data, x0, 1), // Find the index of our data array 
+                  d0 = data[i - 1],
+                  d1 = data[i],
+                  /*d0 is the combination of Year and numberOfAttacks that is in the data array at the index to the left of the cursor 
+                  and d1 is the combination of Year and numberOfAttacks that is in the data array at the index to the right of the cursor.*/
+                  d = x0 - d0.year > d1.year - x0 ? d1 : d0; //d is now the data row for the year closest to the mouse position
+                  focus.select("text").text(function(columnName){
+                     return (formatYear(d.year) + ": " + d[columnName]);
+                  });
+        }; 
+
+});
+        function findMaxY(data){  // Define function "findMaxY"
+          var maxYValues = data.map(function(d) { 
+            if (d.visible){
+              return d3.max(d.values, function(value) { // Return max rating value
+                return value.numberOfAttacks; })
+            }
+          });
+          return d3.max(maxYValues);
+        }
+
+}) (d3);
